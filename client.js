f17dc24c6a63b62066198714324a85f80a8f4fe5
Khalid <Khalid.Ali@consultant.com>
2015-07-04 11:46:57 -0400

    added drawing event and callback

--- client.js
+++ client.js
@@ -1,18 +1,52 @@
 (function() {
+
 	'use strict';
+
+	// connect to socket.io 
 	var socket = io.connect('http://localhost:3000');
+
 	// get elements
-	// canvas elem 
-	canvas = document.getElementById('.drawingPaper'),
+	var canvas = document.getElementBy('drawingPaper'),
 		// access rendering context
 		ctx = canvas.getContext('2d'),
+		// mouse cursor
+		cursor = document.getElementById('cursor'),
 		// game instructions
-		gameRules = document.getElementById('.rules'),
+		gameRules = document.getElementById('rules'),
 		// Generate UUID of 5 characters
-		userID = Math.random() toString(36).subtr(2, 5),
+		user = Math.random() toString(36).subtr(2, 5),
 		// state for drawing activity
-		drawing = false;
-	clients = {},
-		cursors = {};
-		
+		drawing = false,
+		players = {},
+		arrows = {},
+		prev = {};
+	// 
+	canvas.addEventListener('mousedown', function(e) {
+		e.preventDefault();
+		drawing = true;
+
+	});
+ 
+
+	
+	// each time drawing event from server is triggered
+	socket.on('drawing', function(data) {
+		console.log("what's in data : " + data);
+		var newCursor = cursor.innerHTML += "<span class='cursor'> <span>";
+		var moveArrow = arrows[data.user].style;
+		// does players obj have data.user (unique id) key
+		// as a direct property
+		if (players.hasOwnProperty(data.user)) {
+			// build new user's drawing cursor
+			arrows[data.user] = newCursor;
+		}
+		// move mouse pointer
+		moveArrow.left = data.x;
+		moveArrow.top = data.y;
+		// draw
+		draw(players[data.user].x, players[data.user].y, data.x, data.y);
+		// save players states
+		players[data.user] = data;
+
+	});
 })();
