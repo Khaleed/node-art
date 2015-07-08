9c5a8e58a695c67fc89ec247b3794372e0befea5
Khalid <Khalid.Ali@consultant.com>
2015-07-08 13:52:39 -0400

    add primary game logic

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Very simple drawing app using Node, Express, Socket.io and Vanilla JS for the Front-End using HTML5 Canvas. 
+
+Credit: Martin Angelov tutorial on http://tutorialzine.com/2012/08/nodejs-drawing-game/
--- client.js
+++ client.js
@@ -4,38 +4,50 @@
 
 	// connect to socket.io 
 	var socket = io.connect('http://localhost:3000');
-
 	// get elements
-	var canvas = document.getElementBy('drawingPaper'),
-		// access rendering context
-		ctx = canvas.getContext('2d'),
-		// mouse cursor
-		cursor = document.getElementById('cursor'),
-		// game instructions
+	var canvas = document.getElementBy('canvas'),
+		// rendering context
+		ctx,
+		// mouse pointer
+		pointer = document.getElementById('pointer'),
+		// rules
 		gameRules = document.getElementById('rules'),
 		// Generate UUID of 5 characters
-		user = Math.random() toString(36).subtr(2, 5),
+		user = Math.random().toString(36).subtr(2, 5),
+		today = new Date,
+		emittedTime = today.getMilliseconds(),
 		// state for drawing activity
 		drawing = false,
 		players = {},
-		arrows = {},
-		prev = {};
-	// 
+		pointers = {},
+		hist = {};
+	// event handler for drawing on canvas
 	canvas.addEventListener('mousedown', function(e) {
 		e.preventDefault();
 		drawing = true;
-
+		hist.x = e.pageX; // X coordinate (in pixels) of mouse pointer
+		hist.y = e.pageY; // Y coordinate of mouse pointer
+		// hide game rules
+		gameRules.style.display = 'none';
+	});
+	// drawing
+	document.addEventListener('mousemove', function(e) {
+		// figure out a way to reduce 
+	});
+	// not drawing
+	document.addEventListener('mouseup mouseleave', function(e) {
+		drawing = false;
 	});
 	// each time drawing event from server is triggered
 	socket.on('drawing', function(data) {
 		console.log("what's in data : " + data);
-		var newCursor = cursor.innerHTML += "<span class='cursor'> <span>";
-		var moveArrow = arrows[data.user].style;
+		var newCursor = pointer.innerHTML += "<span class='pointer'> <span>";
+		var moveArrow = pointers[data.user].style;
 		// does players obj have data.user (unique id) key
 		// as a direct property
 		if (players.hasOwnProperty(data.user)) {
-			// build new user's drawing cursor
-			arrows[data.user] = newCursor;
+			// build new user's drawing pointer
+			pointers[data.user] = newCursor;
 		}
 		// move mouse pointer
 		moveArrow.left = data.x;
