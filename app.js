11bc77ea238890a708e2a5a4457c302f812c3089
Khalid <Khalid.Ali@consultant.com>
2015-07-12 10:09:30 -0400

    re-arrange file order and re-factor client.js

--- app.js
+++ app.js
@@ -7,13 +7,14 @@
 	var server = require('http').Server(app);
 	// socket.io and htpp sharing the same server
 	var io = require('socket.io')(server);
+	var path = require('path');
 	var port = process.env.port || 3000;
 
 	// routes
 	app.get('/', function(req, res) {
 		res.sendFile(__dirname + '/index.html');
 	});
-	// static routes
+	// static route
 	app.use('/static', express.static('static'));
 
 	// listen to connection event for socket.io
@@ -24,6 +25,7 @@
 			socket.broadcast.emit('moving', data);
 		});
 	});
+	// app listen same as http.Server.listen
 	server.listen(port, function() {
 		console.log('listening on port ' + port);
 	});
